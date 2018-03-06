0b33967df1e82dbe963a1cee647e73dbd7d936c2
Tyler Frankenstein <tfrankenstein@crimewatchus.com>
2018-03-06 16:44:41 -0500

    Always render modal header, but omit title if it wasn't included.

--- src/widgets/widget.modal-header.js
+++ src/widgets/widget.modal-header.js
@@ -1,7 +1,8 @@
 dg.theme_modal_header = function(variables) {
   variables._attributes.class.push('modal-header');
-  return '<div ' + dg.attrs(variables) + '>' +
-      '<span class="modal-close" onclick="dg_modal.close()">&times;</span>' +
-      '<h2>' + variables._title + '</h2>' +
-  '</div>';
+  var html = '<div ' + dg.attrs(variables) + '>' +
+      '<span class="modal-close" onclick="dg_modal.close()">&times;</span>';
+      if (variables._title) { html += '<h2>' + variables._title + '</h2>'; }
+  html += '</div>';
+  return html;
 };
