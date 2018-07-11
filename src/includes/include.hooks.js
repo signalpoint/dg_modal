9b1b12818acbcc4336e941336e436efacb79064d
Tyler Frankenstein <tfrankenstein@crimewatchus.com>
2018-07-11 15:01:04 -0400

    Do modal clean up on pre route change instead of post route change. During the clean up, if the modal is open close it before deleting it.

--- src/includes/include.hooks.js
+++ src/includes/include.hooks.js
@@ -13,18 +13,22 @@ function dg_modal_blocks_build_alter(blocks) {
 /**
  * Implements hook_pre_process_route_change().
  */
-function dg_modal_post_process_route_change(route, newPath, oldPath) {
+function dg_modal_pre_process_route_change(newPath, oldPath) {
   var stack = dg_modal.getStack();
+  var id = null;
   if (stack.length) {
     while (stack.length) {
-      var id = stack.pop();
+      id = stack.pop();
+      if (dg_modal.isOpen(id)) { dg_modal.close(id); }
       dg_modal.delete(id);
     }
   }
   // Clear out any stragglers that aren't on the stack.
   var modals = dg._modals;
-  for (var id in modals) {
+  for (id in modals) {
     if (!modals.hasOwnProperty(id)) { continue; }
+    var modal = modals[id];
+    if (modal.isOpen()) { modal.close(); }
     dg_modal.delete(id);
   }
 }
